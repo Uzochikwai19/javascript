1f3303213adbe00d8a3fe2826479cd354e82ccb9
Uzochikwai19 <alexandranwaduba@gmail.com>
2022-06-24 11:50:19 +0100

    script

---
+++ script.js
@@ -0,0 +1,12 @@
+//alert("Zuri Assignment");
+
+console.log('Name: Uzochikwai Alexandra Nwaduba');
+console.log('Height: 168cm');
+console.log('Country: Nigeria');
+
+//document.write('Name: Uzochikwai Alexandra Nwaduba\n');
+//document.write('Height: 168cm\n');
+//document.write('Country: Nigeria');
+
+
+
